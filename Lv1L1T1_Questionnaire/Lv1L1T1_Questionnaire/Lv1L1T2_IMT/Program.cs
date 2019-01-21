7fa5d494d376971bb0986ae0eb01555ad1ad94eb
EvgeniyKhazanov <EugeniyK@mail.ru>
2019-01-21 08:24:00 +0300

    Добавил задание 2

---
+++ Lv1L1T1_Questionnaire/Lv1L1T1_Questionnaire/Lv1L1T2_IMT/Program.cs
@@ -0,0 +1,24 @@
+﻿// Евгений Хазанов
+//Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах
+
+using System;
+
+namespace Lv1L1T2_IMT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dGrowth;
+            double dWeight;
+            double dIMT;
+            Console.WriteLine("Введите свой рост");
+            dGrowth = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите свой вес");
+            dWeight = Convert.ToDouble(Console.ReadLine());
+            dIMT = dWeight / (dGrowth * dGrowth);
+            Console.WriteLine("Ваш индекс массы тела составляет: "+ dIMT);
+            Console.ReadLine();
+        }
+    }
+}
