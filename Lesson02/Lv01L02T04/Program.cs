1e2d61d60bec033bdf46b8560014ecbafe30af29
EvgeniyKhazanov <EugeniyK@mail.ru>
2019-01-24 11:57:01 +0300

    Сделал 4 задания

---
+++ Lesson02/Lv01L02T04/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Евгений Хазанов
+// Задание 4. Реализовать метод проверки логина и пароля. На вход подается логин и пароль. На выходе истина, если прошел авторизацию, 
+// и ложь, если не прошел (Логин: root, Password: GeekBrains). Используя метод проверки логина и пароля, написать программу: 
+// пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает. 
+// С помощью цикла do while ограничить ввод пароля тремя попытками
+
+namespace Lv01L02T04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            string sLoginTemp = "";
+            string sPasswordTemp="";
+            string sLogin = "root";
+            string sPassword = "GeekBrains";
+            do
+            {
+                Console.Write("Введите логин: ");
+                sLoginTemp = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                sPasswordTemp = Console.ReadLine();
+                if (sLoginTemp == sLogin & sPasswordTemp == sPassword)
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                { 
+                    Console.WriteLine("Ошибка! Количество оставшихся попыток " + (3 - i));
+                    if (i == 3) 
+                    {
+                        Console.WriteLine("До свиданья. Будем прощаться");
+                        Console.ReadLine();
+                        break;
+                    }
+                        else
+                        i++;
+                    
+                }
+            }
+            while (i <= 3);
+        }
+    }
+}
---
+++ Lesson02/Lv01L02T2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Евгений Хазанов
+// Задание 2. Написать программу подсчета количества цифр числа
+
+namespace Lv01L02T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            while (a > 0) 
+            {
+                s = s+1;
+                a = a / 10;
+            }
+            Console.Write("Количество цифр в числе: {0}", s);
+            Console.ReadLine();
+        }
+    }
+}
